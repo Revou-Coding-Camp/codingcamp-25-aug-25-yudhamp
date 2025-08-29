04cba81edee928e71de3b1508d16962a30b489cd
Adhuy <yudhamerdeka91@gmail.com>
2025-08-29 07:11:07 +0700

    Add images and update sections in HTML and CSS for improved layout and design

--- js/script.js
+++ js/script.js
@@ -1,36 +1,51 @@
-// Disabled button submit validation
-const form = document.querySelector('.message-form');
-const submitBtn = document.getElementById('submitBtn');
-const inputs = form.querySelectorAll('input, textarea');
+greet();
 
-function checkInputs() {
-  let hasValue = false;
-  inputs.forEach(input => {
-    if (
-      (input.type === 'radio' && form.querySelector('input[name="gender"]:checked')) ||
-      (input.type !== 'radio' && input.value.trim() !== '')
-    ) {
-      hasValue = true;
-    }
-  });
-  submitBtn.disabled = !hasValue;
+function greet() {
+  let username = prompt("Please insert your name !");
+  // Update text welcome
+  const welcomeText = document.getElementById('welcomeTitle');
+  welcomeText.innerText = `Hi ${username || ''},`;
 }
 
-inputs.forEach(input => {
-  input.addEventListener('input', checkInputs);
-  if (input.type === 'radio') {
-    input.addEventListener('change', checkInputs);
-  }
-});
-
-
-document.querySelector('.message-form').addEventListener('submit', function(event) {
-  event.preventDefault(); 
-
+// Form validation
+document.getElementById('submitBtn').addEventListener('click', () => {
   // Get form values
   const name = document.getElementById('name').value;
   const date = document.getElementById('date').value;
-    const gender = document.querySelector('input[name="gender"]:checked')?.value === 'male' ? 'Laki-laki' : 'Perempuan';
+  const gender = document.querySelector('input[name="gender"]:checked')?.value;
+  const message = document.getElementById('message').value;
+
+  if (name === '') {
+    alert('Silakan masukkan nama anda!');
+  } 
+  else if (date === '') {
+    alert('Silakan masukkan tanggal lahir anda!');
+  } 
+  else if (gender === '' || gender === undefined) {
+    alert('Silakan pilih jenis kelamin anda!');
+  }
+  else if (message === '') {
+    alert('Silakan masukkan pesan anda!');
+  }else {
+    return;
+  }
+});
+
+// Handle submit
+document.querySelector('.message-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get form values
+  const name = document.getElementById('name').value;
+  let dateValue = document.getElementById('date').value;
+  // Format date to dd/mm/yyyy
+  let date = '';
+  if (dateValue) {
+    const [year, month, day] = dateValue.split('-');
+    date = `${day}/${month}/${year}`;
+  }
+
+  const gender = document.querySelector('input[name="gender"]:checked')?.value === 'male' ? 'Laki-laki' : 'Perempuan';
   const message = document.getElementById('message').value;
   const currentTime = new Date().toString();
 
@@ -59,17 +74,14 @@ document.querySelector('.message-form').addEventListener('submit', function(even
     </p>
   `;
 
-  // Update text welcome
-  const welcomeText = document.getElementById('welcomeTitle');
-  welcomeText.innerHTML = `
-    Hi <span style="text-transform: capitalize;">${name}<span>, Welcome to Website
-  `;
-
   alert('Input data berhasil !');
   window.scrollTo({
     top: document.body.scrollHeight,
     behavior: 'smooth'
   });
+
+  // Reset form
+  document.querySelector('.message-form').reset();
 });
 
 // handle button menu
